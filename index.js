351f892f29a4247945b5dd8860282228201488f3
Basavakumar Mudur <Basavakumar.Mudur@INDECOMM.net>
2019-01-08 23:48:15 +0530

    initial setup of react native

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import { AppRegistry } from "react-native";
+import App from "./src/components/App";
+import { name as solar_topps } from "./app.json";
+
+AppRegistry.registerComponent(solar_topps, () => App);
